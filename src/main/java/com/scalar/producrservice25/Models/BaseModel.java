cac5e9b18875ded9d1e2ffc0f0a9c87acbc469ae
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-05-29 20:35:33 +0530

    Added the dependencies for springboot JPA and MySql connector from the mvnrepository website. So that -> to work with mysql database (own database). Created a new database from mysql workbench -> productservice25 Added the configurations for the database(mysql) ->Spring data Jpa Connecting with mySql -> in the bapplication.properties (-> in the resources folder/package) Added @Entity -> to tell springboot that -> create a table for this model ORM -> Object Relational Model -> will create tables from models -> we dont have to do it manually -> ORM -> Hibernate -> does it for us Added @Id -> to tell it is primary key in the tables Added @GenerativeValue(stategy = GenerationType.IDENTITY) -> telling spring to create the auto increments of Id Added @M<appedSuperClass -> Telling Spring that this Model is parent , dont create table for this model, But for the attributes in this model -> the child tabvles will get all the attributes of the parent model(Inheritance) Added @ManyToOne Cardinality -> telling spring that There is Many to one mapping between Product and Category And Application was successfully run -> Created tables Can check the tables in the database in IntellijIdea Ultimate version To add database in this intellij -> database -> + -> data source -> MySql(as we are using mysql database) -> Enter tha database name that we created in the sql workbench -> productservice25 And Enter the user name that we created in the sql workbench -> productservice25user -> Test connection -> Apply -> OK. Now the tables are created -> can be seen in database -> Intellij -> productservice25 .. We nammed our tabvles  -> Product tables as products -> @Entity(name = products) category tables as categories -> @Entity(name=categories) Naming our tables is not mandatory. If I dont name Product table -> It takes the name of the model as table name -> it takes Product as name .

--- src/main/java/com/scalar/producrservice25/Models/BaseModel.java
+++ src/main/java/com/scalar/producrservice25/Models/BaseModel.java
@@ -1,5 +1,9 @@
 package com.scalar.producrservice25.Models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +11,10 @@ import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
+    @Id //telling Id is primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto Increments the Id
     private Long id;
     private Date createdAt;
     private Date updatedAt;
--- src/main/java/com/scalar/producrservice25/Models/Product.java
+++ src/main/java/com/scalar/producrservice25/Models/Product.java
@@ -1,14 +1,19 @@
 package com.scalar.producrservice25.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "products")
 public class Product extends BaseModel {
     private String title;
     private String description;
     private String imageUrl;
     private double price;
+
+    @ManyToOne
     private Category category;
 }
