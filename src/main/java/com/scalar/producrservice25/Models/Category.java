1b222a33fb9ec008fd2cb9f0f2c6a51a0fc0ec49
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-05-30 23:10:39 +0530

    Added mappedBy -> to @OneToMany in categiry model -> to tell spring that this cardinality connection was already made in Products table at 'category' attribute.

--- src/main/java/com/scalar/producrservice25/Models/Category.java
+++ src/main/java/com/scalar/producrservice25/Models/Category.java
@@ -1,12 +1,18 @@
 package com.scalar.producrservice25.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity(name = "categories")
 public class Category extends BaseModel {
     private String name;
+
+    @OneToMany(mappedBy = "category") //This mappedBy -> can only be present in  OneToMany side.
+    List<Product> products;
 }
