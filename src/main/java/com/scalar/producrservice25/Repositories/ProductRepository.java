0ac7eadb58051ebe44a0015610f84888ac078baa
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-05-30 15:49:22 +0530

    Added ProductRepository interface in repositories folder. Added some methods to fetch data from sql data base in the interface -> findById, findAll, ... Injected thgis ProductRepository in selfProductService class -> so that it process the data from db and gives it to service class where business logic resides. Using Optional<> -> This is from JPARepository -> extending in ProductRepository Interface Optional<> is lika a bag. If we get the dta from db , and the product we got is null, and if we do product.name -> it gives error. So with Optional we get compile checks. -> get handle the error here itself (Exceptions) here in the db, we created a product and category manually -> to check the code. Its working .

---
+++ src/main/java/com/scalar/producrservice25/Repositories/ProductRepository.java
@@ -0,0 +1,26 @@
+package com.scalar.producrservice25.Repositories;
+
+import com.scalar.producrservice25.Models.Category;
+import com.scalar.producrservice25.Models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Override
+    Optional<Product> findById(Long productId);
+    //select * from products where isd = productId;
+
+    @Override
+    List<Product> findAll();
+
+    Optional<Product> findByTitleContains(String str);
+    //select * from products where tile like '%str%';
+
+    Optional<Product> findByCategory(Category category);
+
+    Optional<Product> findByCategory_Id(Long id);
+}
--- src/main/java/com/scalar/producrservice25/services/SelfProductService.java
+++ src/main/java/com/scalar/producrservice25/services/SelfProductService.java
@@ -1,17 +1,30 @@
 package com.scalar.producrservice25.services;
 
 import com.scalar.producrservice25.Models.Product;
+import com.scalar.producrservice25.Repositories.ProductRepository;
 import com.scalar.producrservice25.exceptions.ProductNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service("selfproductservice")
 //@Primary
 public class SelfProductService implements ProductService {
+    private ProductRepository productRepository;
+    public SelfProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product getSingleProduct(Long id) throws ProductNotFoundException {
-        return new Product();
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if(optionalProduct.isEmpty()){
+            throw new ProductNotFoundException("Product with given id -> " + id + " is not found");
+        }
+        Product product = optionalProduct.get();
+        return product;
     }
 
     @Override
